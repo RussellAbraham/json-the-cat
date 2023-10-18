edb74b5ad54c9da74e1610222103244900eded20
RussellAbraham <russell_a@live.com>
2023-10-17 19:08:04 -0700

    Update breedFetcher to take arguments and parse string to an object

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# json-the-cat
+# JSON The Cat
+
+Cats API command line application
+
+## Usage
+
+- `node breedFetcher.js sib`
+- `node breedFetcher.js Chartreux`
+
+Prints out the selected breed of cat.
--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,18 @@
 const request = require('request');
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else if (response.statusCode !== 200) {
-    console.error(response.statusCode);
-  } else {
-    console.log(body);
-  }
-});
+const args = process.argv.slice(2);
 
+const fetchBreed = (url) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${url}`, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else if (response.statusCode !== 200) {
+      console.error(response.statusCode);
+    } else {
+      const data = JSON.parse(body)
+      console.log(data);
+    }
+  });
+};
+
+fetchBreed(args[0]);
